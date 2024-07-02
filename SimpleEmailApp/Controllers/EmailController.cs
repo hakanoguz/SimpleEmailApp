a2605232bbb6f700c35ae24b669473c71e7cd8c2
hakan.oguz <hakan.oguz@TFS-27494>
2024-07-02 15:44:26 -0500

    Add project files.

---
+++ SimpleEmailApp/Controllers/EmailController.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Mail;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+
+namespace SimpleEmailApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private IEmailService _emailService;
+
+        public EmailController(IEmailService emailService) 
+        { 
+            _emailService = emailService;
+        }
+
+        [HttpPost]
+        public IActionResult SendEmail(EmailDto request)
+        {
+            _emailService.SendEmail(request);
+
+            return Ok();   
+
+
+        }
+
+    }
+}
---
+++ SimpleEmailApp/Services/EmailService/EmailService.cs
@@ -0,0 +1,29 @@
+﻿
+using MimeKit;
+
+namespace SimpleEmailApp.Services.EmailService
+{
+    public class EmailService : IEmailService
+    {
+        private IConfiguration _config;
+
+        public EmailService(IConfiguration _config) 
+        {
+            this._config = _config;
+        }
+        public void SendEmail(EmailDto request)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(_config.GetSection("EmailUsername").Value));
+            email.To.Add(MailboxAddress.Parse(request.To));
+            email.Subject = request.Subject;
+            email.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = request.Body };
+
+            using var smtp = new MailKit.Net.Smtp.SmtpClient();
+            smtp.Connect(_config.GetSection("EmailHost").Value, 587, MailKit.Security.SecureSocketOptions.StartTls);
+            smtp.Authenticate(_config.GetSection("EmailUsername").Value, _config.GetSection("EmailPassword").Value);
+            smtp.Send(email);
+            smtp.Disconnect(true);
+        }
+    }
+}
